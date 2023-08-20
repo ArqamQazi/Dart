e9d600f7eb7bd0c51dd3757a6dd2373f7bc24912
ArqamQazi <arqamqazi549@gmail.com>
2023-08-20 07:33:33 +0530

    Varible

---
+++ C4_Vaiable2.dart
@@ -0,0 +1,12 @@
+void main() {
+  // Variable
+  // var/final/const varibleName = value;
+  
+  var someValue1 = "10";
+  final someValue2 = "10";
+  const someValue3 = "10";
+  
+  print(someValue1);
+  print(someValue2);
+  print(someValue3);
+}
